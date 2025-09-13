c36ab7beab03c594cd5c9637e249e9c9cb574786
William Hunt <phunt22@uw.edu>
2025-09-12 22:08:07 -0400

    init vite

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+import requests
+import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+predict_hq_token = os.getenv("PREDICTHQ_TOKEN")
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
+
+
+# more methods here
+
+
+@app.get("twsift-test")
+async def test():
+    response = requests.get(
+        url="https://api.predicthq.com/v1/events/",
+        headers={
+        "Authorization": f"Bearer {predict_hq_token}",
+        "Accept": "application/json"
+        },
+        params={
+            "q": "taylor swift"
+        }
+    )
+
+    data = response.json()
+
+    return {"data": data}
