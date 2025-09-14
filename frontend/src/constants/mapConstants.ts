3c187d33ccfda7e7d1049d5038ee90f79a1cea21
William Hunt <phunt22@uw.edu>
2025-09-13 23:46:31 -0400

    world view over baltimore

--- frontend/src/constants/mapConstants.ts
+++ frontend/src/constants/mapConstants.ts
@@ -2,7 +2,7 @@
 export const DEFAULT_STYLE = "https://demotiles.maplibre.org/style.json";
 
 // BMORE VIEW
-export const DEFAULT_VIEW = { lon: -76.3614, lat: 39.1740, zoom: 4, bearing: 0, pitch: 0 };
+export const DEFAULT_VIEW = { lon: -76.3614, lat: 39.1740, zoom: 1.5, bearing: 0, pitch: 0 };
 
 // WORLD VIEWs
 export const WORLD_VIEW = { lon: 0, lat: 20, zoom: 1, bearing: 0, pitch: 0 };
