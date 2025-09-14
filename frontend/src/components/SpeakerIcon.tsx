7dd0fad3342ce815d163052357995468900077b5
litladeji <ladejiemma4real@gmail.com>
2025-09-14 01:21:32 -0400

    changed the microphone and speaker icons

--- frontend/src/components/SpeakerIcon.tsx
+++ frontend/src/components/SpeakerIcon.tsx
@@ -35,9 +35,11 @@ export default function SpeakerIcon({
         strokeLinecap="round" 
         strokeLinejoin="round"
       >
-        <path d="M12 2L2 7l10 5 10-5-10-5z" />
-        <path d="M2 17l10 5 10-5" />
-        <path d="M2 12l10 5 10-5" />
+        {/* Microphone icon for dictation/speech-to-text */}
+        <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
+        <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+        <line x1="12" y1="19" x2="12" y2="23" />
+        <line x1="8" y1="23" x2="16" y2="23" />
       </svg>
     </button>
   );
